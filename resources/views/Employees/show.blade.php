8b35473b1e2337c40351f898a730f534377e4709
marlontamo <marlontamo@gmail.com>
2022-03-24 03:11:11 -0700

    Added Bootstrap in the test

--- resources/views/Employees/show.blade.php
+++ resources/views/Employees/show.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="/employee" class="btn-success">Back to List</a>
+<a href="/employee" class="btn btn-success">Back to List</a>
 <div class="row">
     
     <h1>Employee Profile ID Number:{{$employee->id}}</h1>
     <div class="card" style="width:400px">
     <div class="card-body">
-      <h4 class="card-title">{{$employee->last_name}},{{$employee->first_name}}</h4>
+      <h4 class="card-title">Fullname: {{$employee->last_name}},{{$employee->first_name}}</h4>
       <p class="card-text">Email:{{$employee->email}}</p>
       <p class="card-text">Position:{{$employee->title}}</p>
       <p class="card-text">Country: {{$employee->country}}</p>
