ae4d28c638c030c9f98493da397e8348ed64ed52
marlontamo <marlontamo@gmail.com>
2022-03-24 19:21:13 -0700

    added font-family

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,11 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+<style>
+    body{
+        font-family: sans-serif;
+    }
+</style>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
